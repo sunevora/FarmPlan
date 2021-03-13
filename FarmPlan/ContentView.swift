2f98f57eee0907cfd87ba6004c8f908e3f420af3
Thorsten Karrer <git@thorsten-karrer.com>
2021-03-13 14:33:20 +0100

    Initial Commit

---
+++ FarmPlan/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FarmPlan
+//
+//  Created by Thorsten Karrer on 13.03.21.
+//  Copyright © 2021 Thorsten Karrer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
